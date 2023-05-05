45d8237a3cca8694941ab99f434fa674d20e73c3
grafika10 <grafika10@ucione.local>
2023-05-05 21:40:26 +0200

    new class added

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
 //1. dohvatiti box i promijeniti background color
 const box = document.querySelector(".box");
-box.style.backgroundColor = "red";
+// box.style.backgroundColor = "red";
 
 //2. dodati dodatnu klasu na box koja će povećati njegove dimenzije
-const list = box.classList.add("dimension");
+// const list = box.classList.add("dimension");
 
 //3. na klik animirati box do 30px i nazad
 
@@ -34,26 +34,103 @@ const list = box.classList.add("dimension");
 // }, []);
 
 //4. kreirati funkciju koja će imati lokalnu varijablu u kojoj ćemo izračunati sumu dva parametra i rezultat ispisati unutar box-a
-const sum = (num1, num2) => {
-  let twoNumSum = num1 + num2;
-  return twoNumSum;
-};
-box.innerHTML = sum(2, 5);
+// const sum = (num1, num2) => {
+//   let twoNumSum = num1 + num2;
+//   return twoNumSum;
+// };
+// box.innerHTML = sum(2, 5);
 
 //5. animirati box dijagonalno do (30px, 30px) i nazad do 50px
-let positionD = 0;
-let horizonD = 0;
-let reverseD = false;
 
-const moveDiagonally = () => {
-  positionD++;
-  let id = setInterval(frame, 30);
+const container = document.querySelector(".container");
 
-  const frame = () =>{
-    if
-  }
-};
+// let positionD = 0;
+// let test = true;
+
+// const moveDiagonally = () => {
+//   box.style.left = positionD + "px";
+//   box.style.top = positionD + "px";
+
+//   if (test) {
+//     positionD++;
+//   } else {
+//     positionD--;
+//   }
+
+//   if (positionD >= 150) {
+//     test = false;
+//   } else if (positionD <= 0) {
+//     test = true;
+//   }
+// };
+
+// let t = setInterval(moveDiagonally, 1);
+
 //6. dodaj onclick event i smanji button
-const handleClick = () => {
-  const smallerBtn = document.querySelector(".btn").classList.add("smaller");
-};
+// const handleClick = () => {
+//   const smallerBtn = document.querySelector(".btn").classList.add("smaller");
+// };
+
+//Create a class Dog
+class Dog {
+  constructor(hasSat, bark, doSit, standUp) {
+    this.hasSat = hasSat;
+    this.bark = bark;
+    this.doSit = doSit;
+    this.standUp = standUp;
+
+    this.hasSat = false;
+    this.bark = function () {
+      return console.log("vau vau");
+    };
+    this.doSit = function () {
+      this.hasSat = true;
+    };
+    this.standUp = function () {
+      this.hasSat = false;
+    };
+  }
+}
+//Create an instance of Dog class
+const dog = new Dog();
+console.log(dog.hasSat);
+dog.bark();
+dog.doSit();
+console.log(dog.hasSat);
+dog.bark();
+
+//7. Zadatak kreiraj klasu Room koja će imati vrijednosti
+//širina, visina i dužina i metode za racunanje površine
+//i prostora sobe. Dodati još vrijednost je li upaljeno
+//svjetlo i metodu koja će nam u konzolu ispisati trenutno
+//stanje svjetla
+
+class Room {
+  constructor(width, height, length, light) {
+    this.width = width;
+    this.height = height;
+    this.length = length;
+    this.light = light;
+  }
+  light = false;
+
+  calcArea() {
+    return console.log(this.height * this.width);
+  }
+  calcSpace() {
+    return console.log(this.height * this.width * this.length);
+  }
+  showLight() {
+    if (this.light == true) {
+      return console.log("The light is on.");
+    } else {
+      return console.log("The light is off.");
+    }
+  }
+}
+
+const room = new Room(10, 3, 6);
+
+room.calcArea();
+room.calcSpace();
+room.showLight();
